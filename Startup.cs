4e9cfd3657846f3c9917aa5ba8b1a4b74ba99d7a
joaoAngeloJr <angelojr10@gmail.com>
2020-07-26 23:11:24 -0300

    CORS policy issue resolved

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using great_challenge.Abstract;
 using great_challenge.Repository;
@@ -23,12 +24,26 @@ namespace great_challenge
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "GreatChallengeReact";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<greatContext>(options => 
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Parse("HOST_IP_ADDRESS"));
+            });
+
+            services.AddCors(options =>
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder => builder
+                    .WithOrigins("HOST_ADDRESS")
+                    .AllowAnyOrigin()
+                    .WithMethods("GET", "POST", "PUT", "DELETE")
+                    .WithHeaders("Access-Control-Allow-Headers", "accept", "content-type", "origin")));
+
+            services.AddDbContext<greatContext>(options =>
             options.UseSqlServer(@"Server=LOCAL_DESKTOP;Database=DB_NAME;Trusted_Connection=True;"));
 
             services.AddControllers();
@@ -46,6 +61,7 @@ namespace great_challenge
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseRouting();
 
