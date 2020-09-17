8fb3bf06b3c3248c9cf1e96b7831fee15bea244d
JoaoAngelojr <angelojr10@gmail.com>
2020-09-17 14:51:23 -0300

    auth migration

---
+++ Migrations/20200917174059_UserNameAndPassword.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace great_challenge.Migrations
+{
+    public partial class UserNameAndPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
